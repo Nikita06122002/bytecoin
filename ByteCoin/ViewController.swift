5c8c29debb5900dea1ee6e47ae6d17ca3a60f76b
macbook <yakovenko06122002@icloud.com>
2023-11-23 18:39:12 +0300

    Initial commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by macbook on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
